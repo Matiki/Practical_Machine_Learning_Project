da2a922fb5620cd9728b0a91db44aa38cbf7cdd1
Matiki <matiasbruzza@gmail.com>
2018-09-28 21:41:46 -0700

    removed columns with too many NAs

--- analysis.R
+++ analysis.R
@@ -1,6 +1,7 @@
 # Load necessary R packages into current R session
 library(readr)
 library(caret)
+library(dplyr)
 
 # Check if data files exist in working directory, if not: download the data
 if(!file.exists("pml-training.csv")){
@@ -25,16 +26,32 @@ testing <- read_csv("pml-testing.csv")
 head(training); str(training); summary(training)
 
 # split data into train/test sets
+set.seed(54321)
 inTrain <- createDataPartition(y = training$classe, p = .75, list = FALSE)
-train <- training[inTrain]
-test <- training[-inTrain]
+trainset <- training[inTrain, ]
+testset <- training[-inTrain, ]
+rm(inTrain)
+
+# pre processing:
+# remove unnecessary variables related to ID
+trainset <- trainset[, -c(1:7)]
+
+# remove variables with nearly zero variance
+nzv <- nearZeroVar(trainset, saveMetrics = TRUE)
+trainset <- trainset[, !nzv$nzv]
+rm(nzv)
+
+# remove rows with greater than 50% NA's
+mostlyNA <- sapply(trainset, is.na) %>%
+        colMeans() > 0.5
+trainset <- trainset[!mostlyNA]
 
 # fit a decision tree model
-model_tree <- train(classe ~., data = training, method = "rpart")
+model_tree <- train(classe ~., data = trainset, method = "rpart")
 
 # fit a random forest model
-model_rf <- train(classe ~., data = training, method = "rf")
+model_rf <- train(classe ~., data = trainset, method = "rf")
 
 # fit boosting model 
-model_boost <- train(classe ~., data = training, method = "gbm")
+model_boost <- train(classe ~., data = trainset, method = "gbm")
 
